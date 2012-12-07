c5d4ffe56268704c9140b6e112b7015f4f0ed052
jepl <j.pletniov@gmail.com>
2012-12-07 12:38:32 +0200

    naujas langas

---
+++ TestProject/Ololo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Interaction logic for Ololo.xaml
+    /// </summary>
+    public partial class Ololo : Window
+    {
+        public Ololo()
+        {
+            InitializeComponent();
+        }
+    }
+}
