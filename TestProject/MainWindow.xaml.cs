a8ced52afc903b534e8f999a4984df5846085a45
dluksa <d.luksa@baltic-amadeus.lt>
2012-12-07 13:10:54 +0200

    a

--- TestProject/MainWindow.xaml.cs
+++ TestProject/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace TestProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
